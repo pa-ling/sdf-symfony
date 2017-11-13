493c99200b0ac97fc2adea17941014ac7d20157f
Patrick Fehling <pa-ling@users.noreply.github.com>
2017-11-13 18:05:28 +0100

    add backend for basket operations

--- src/AppBundle/Controller/BasketController.php
+++ src/AppBundle/Controller/BasketController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use AppBundle\Form\MailType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,30 +14,89 @@ class BasketController extends Controller
 {
 
     /**
-     * @Route("/basket", name="basket")
+     * @Route("/basket", name="getBasket")
+     * @Method({"GET"})
      */
-    public function basketAction(Request $request)
-    {
-        $basketItems = array("12345uztukgsedgr", "ewsrgjhu45erg", "324z5hrtdv", "123rwetrz5urhsfdsfvc");
-        $json = json_encode($basketItems);
-        $cookie = new Cookie('basket', $json, time() + (3600 * 48));
-        
-        $cookie = $request->cookies->get('basket');
-        $cookie = stripslashes($cookie);
-        $basketItems = json_decode($cookie, true);
+    public function listBasketItems(Request $request)
+    {        
+        $basketItems = $this->getCookieContent($request->cookies->get('basket'));
 
         $response = $this->render('default/basket.html.twig' , array(
             'basketItems' => $basketItems
-        ));
-
-        //$response->headers->setCookie($cookie);
+        ));;
 
         return $response;
     }
 
-
-    public function addBasketItem(Request $request)
+    /**
+     * @Route("/basket/{id}", name="PostBasket")
+     * @Method({"POST"})
+     */
+    public function addBasketItem(Request $request, $id)
     {
-        
+        //TODO: check in database if id exists
+        //TODO: ids have to be unique in the basket
+
+        $response = new Response();
+        $response->setStatusCode(Response::HTTP_OK);
+
+        $basketItems = $this->getCookieContent($request->cookies->get('basket'));
+
+        if (!$basketItems)
+        {
+            $basketItems = array($id);
+        }
+        else
+        {
+            array_push($basketItems, $id);
+        }
+
+        $response->headers->setCookie($this->createCookie($basketItems, "basket"));
+
+        return $response;
     }
+
+    /**
+     * @Route("/basket/{id}", name="DeleteBasket")
+     * @Method({"DELETE"})
+     */
+    public function deleteBasketItem(Request $request, $id)
+    {
+        $response = new Response();
+        $basketItems = $this->getCookieContent($request->cookies->get('basket'));
+        if (!$basketItems) //if there is no basket
+        {
+            $response->setStatusCode(Response::HTTP_NOT_FOUND);
+            return $response;
+        }
+
+        $basketItemKey = array_search($id, $basketItems);
+        if (!$basketItemKey) //if the item could not be found
+        {
+            $response->setStatusCode(Response::HTTP_NOT_FOUND);
+            return $response;
+        }
+
+        array_splice($basketItems, $basketItemKey, 1);
+
+        $response->setStatusCode(Response::HTTP_OK);
+        $response->headers->setCookie($this->createCookie($basketItems, "basket"));
+
+        return $response;
+    }
+
+    private function getCookieContent($cookie)
+    {
+        $cookie = stripslashes($cookie);
+        $basketItems = json_decode($cookie, true);
+        return $basketItems;
+    }
+
+    private function createCookie($array , $key)
+    {
+        $json = json_encode($array);
+        $cookie = new Cookie($key, $json, time() + (3600 * 48));
+        return $cookie;
+    }
+
 }
