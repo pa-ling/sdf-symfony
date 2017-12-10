f0978440f1a80480a551d9af70ea0c1e9341dedb
Yoggi Firmanda <yoggifirmanda@gmail.com>
2017-12-10 01:32:07 +0100

    New entity

---
+++ src/AppBundle/Entity/GalleryImage.php
@@ -0,0 +1,196 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * GalleryImage
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class GalleryImage
+{   
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="gallery_id", type="integer", nullable=false)
+     */
+    protected $gallery_id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="media_id", type="integer", nullable=false)
+     */
+    protected $media_id;
+
+     /**
+     * @var integer
+     *
+     * @ORM\Column(name="position", type="integer", nullable=true)
+     */
+    protected $position;
+
+    /**
+     * @var bool
+     * 
+     * @ORM\Column(name="enabled", type="boolean", nullable=true)
+     */
+    protected $enabled;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="owned_by", type="integer")
+     */
+    protected $owned_by;
+
+    /**
+     * @var datetime $createdAt
+     *
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $createdAt;
+
+    /**
+     * @var datetime $updatedAt
+     *
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    public function prePersist()
+    {
+        $this->setCreatedAt(new \DateTime());
+        $this->setUpdatedAt(new \DateTime());
+    }
+
+    public function preUpdate()
+    {
+        $this->setUpdatedAt(new \DateTime());
+    }
+    
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setGalleryId($gallery_id)
+    {
+        $this->gallery_id = $gallery_id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getGalleryId()
+    {
+        return $this->gallery_id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setMediaId($media_id)
+    {
+        $this->media_id = $media_id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMediaId()
+    {
+        return $this->media_id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setOwnedBy($owned_by)
+    {
+        $this->owned_by = $owned_by;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOwnedBy()
+    {
+        return $this->owned_by;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUpdatedAt(\DateTime $updatedAt = null)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCreatedAt(\DateTime $createdAt = null)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+}
