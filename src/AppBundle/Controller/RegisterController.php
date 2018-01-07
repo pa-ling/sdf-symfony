028e095ee5d085841c2c5176226a4f51065389a4
Nobody1809 <s0533422@htw-berlin.de>
2018-01-07 16:17:33 +0100

    Registration Default Photograp

--- src/AppBundle/Controller/RegisterController.php
+++ src/AppBundle/Controller/RegisterController.php
@@ -2,6 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Image;
+use AppBundle\Entity\GalleryMedia;
+use AppBundle\Form\ImageType;
+use AppBundle\Service\FileUploader;
+use Application\Sonata\MediaBundle\Entity\Media;
 use AppBundle\Form\UserType;
 use Application\Sonata\UserBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -52,56 +57,4 @@ class RegisterController extends Controller
         );
     }
 
-
-    /**
-     * @Route("/photograph_registration", name="photograph_registration")
-     */
-    public function photographRegisterAction(Request $request)
-    {
-
-        $user = new User();
-
-        $form = $this->createForm(new UserType(), $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPlainPassword());
-            $user->setPassword($password);
-
-            $user->setRoles(array('ROLE_PHOTOGRAPH'));
-            $user->addRole('ROLE_PHOTOGRAPH');
-            $user->setEnabled(true);
-
-            try{
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($user);
-                $em->flush();
-
-            } catch(\Doctrine\DBAL\DBALException $e) {
-                $this->get('session')->getFlashBag()->add('error', 'Can\'t insert entity.');
-
-            }
-
-            $photographers = new Photographers();
-            $photographers->setUserId($user->getId());
-            $photographers->setLongdescr("long des");
-            $photographers->setShortdescr("long des");
-            $photographers->setSurname("Dumke");
-            $photographers->setFirstname("Tobias");
-
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($photographers);
-            $em->flush();
-
-        }
-
-        return $this->render(
-            'security/photograph_register.html.twig',
-            array(
-                'form' => $form->createView(),
-
-            )
-        );
-    }
 }
