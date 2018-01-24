b9ea075b11f5bb9fada5d64a7d710152a3b64ce4
Yoggi Firmanda <yoggifirmanda@gmail.com>
2018-01-24 14:07:52 +0100

    Populate product to download

--- src/AppBundle/Controller/PurchaseController.php
+++ src/AppBundle/Controller/PurchaseController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Entity\Product;
 use \Datetime;
+use ZipArchive;
 
 class PurchaseController extends Controller
 {
@@ -122,4 +123,89 @@ class PurchaseController extends Controller
         return $this->redirectToRoute('purchaseasseller');
     }
 
+    /**
+     * @Route("/download_purchase", name="download_purchase")
+     * 
+     */
+    public function downloadPurchase(Request $request)
+    {
+        $data = $request->request->all();
+        $userId =  $data['userId'];
+        $purchaseId =  $data['purchaseId'];
+
+        // Get purchase data
+        $purchase = $this->getDoctrine()
+            ->getRepository(Purchase::class)
+            ->findOneBy([ 'id' => $purchaseId, 'user'=>$userId]);
+
+        if (!$purchase) {
+            throw $this->createNotFoundException(
+                'No purchase found for id '.$purchaseId
+            );
+        }
+
+        $product = $purchase->getProducts()[0];
+
+        // Check if purchase already paid
+        $isPaid = $purchase->isPaid();
+
+        if($isPaid===true){
+            $productId = $product->getId();
+            $product = $this->getDoctrine()
+                ->getRepository('AppBundle:Product')
+                ->findOneBy(
+                    ['id' => $productId]
+                );
+
+            if (!$product) {
+                throw $this->createNotFoundException(
+                    'No product found for id '.$productId
+                );
+            }
+
+            $galleries = $product->getGallery();
+			$imageIdInGallery = array();
+			foreach ($galleries as $key => $value) {
+				// fetch all gallery_media
+                $gallery_media_fetch = $this->getDoctrine()
+                ->getRepository('AppBundle:GalleryMedia')
+				->findBy(
+					['gallery_id' => $value],
+					['createdAt' => 'DESC']                   
+				);
+            
+				foreach ($gallery_media_fetch as $key => $value) {
+					$media_id = $value->getMediaId();
+					array_push($imageIdInGallery, $media_id); 
+				}
+            }
+            
+            $app_url = $this->getParameter('app_url');
+            $images = array(); 
+            $images_url = array();
+			for ($i=0; $i <sizeof($imageIdInGallery); $i++) { 
+				$images[$i] = $this->get('sonata.media.manager.media')
+					->findOneBy(
+						['id'=>$imageIdInGallery[$i]]
+                    );
+                $images_url[$i] = $app_url.'/uploads/media/default/0001/01/'.$images[$i]->getProviderReference();
+            }
+
+            /**
+             * TODO
+             * Download as ZIP
+             */
+            $files = $images_url;
+            $zipname = $userId.'-purchase.zip';
+            $zip = new ZipArchive;
+            $zip->open($zipname, ZipArchive::CREATE);
+            foreach ( $files as $file) {
+                $zip->addFile($file);
+            }
+            $zip->close();
+
+            return $this->redirect('/purchase');
+        }
+
+    }
 }
